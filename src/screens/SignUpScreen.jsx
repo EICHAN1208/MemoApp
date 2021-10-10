8e9db07f2357a44c9e5ce2faadc94c99d668083b
murakami <ek.h1.hy@gmail.com>
2021-10-10 13:11:50 +0900

    Create LoginScreen and SignUpScreen

---
+++ src/screens/SignUpScreen.jsx
@@ -0,0 +1,77 @@
+import React from 'react';
+import {
+  View, Text, StyleSheet, TextInput,
+} from 'react-native';
+
+import AppBar from '../components/AppBar';
+import Button from '../components/Button';
+
+export default function SignUpScreen() {
+  return (
+    <View style={styles.container}>
+      <AppBar />
+      <View style={styles.inner}>
+        <Text style={styles.title}>Sign Up</Text>
+        <TextInput style={styles.input} value="Email Address" />
+        <TextInput style={styles.input} value="Password" />
+        <Button label="Submit" />
+        <View style={styles.footer}>
+          <Text style={styles.footerText}>Already registered?</Text>
+          <Text style={styles.footerLink}>Log In.</Text>
+        </View>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#F0F4F8',
+  },
+  inner: {
+    paddingHorizontal: 27,
+    paddingVertical: 24,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    lineHeight: 32,
+    marginBottom: 24,
+  },
+  input: {
+    fontSize: 16,
+    height: 48,
+    backgroundColor: '#ffffff',
+    borderColor: '#DDDDDD',
+    borderWidth: 1,
+    paddingHorizontal: 8,
+    marginBottom: 16,
+  },
+  buttonContainer: {
+    backgroundColor: '#467FD3',
+    borderRadius: 4,
+    alignSelf: 'flex-start',
+    marginBottom: 24,
+  },
+  buttonLabel: {
+    fontSize: 16,
+    lineHeight: 32,
+    paddingVertical: 8,
+    paddingHorizontal: 32,
+    color: '#ffffff',
+  },
+  footerText: {
+    fontSize: 14,
+    lineHeight: 24,
+    marginRight: 8,
+  },
+  footerLink: {
+    fontSize: 14,
+    lineHeight: 24,
+    color: '#467FD3',
+  },
+  footer: {
+    flexDirection: 'row',
+  },
+});
